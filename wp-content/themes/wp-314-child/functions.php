85428df2e86ed7782f0f2495e82ec2ba25247353
ghost717 <dawid.zawadzki7717@gmail.com>
2019-09-09 16:48:17 +0200

    CPT

--- wp-content/themes/wp-314-child/functions.php
+++ wp-content/themes/wp-314-child/functions.php
@@ -15,3 +15,84 @@ function image_sizes(){
     add_image_size('fullhd', 1920, 1080, true);
     add_image_size('thumb600', 600, 600, true);
 }
+
+// CUSTOM POST TYPES
+function post_type_zabiegi(){
+    $labels = array(
+        'name' => _x('Zabiegi', 'Zabiegi', 'webs'),
+        'singular_name' => _x('Zabieg', 'Zabieg', 'webs'),
+    );
+
+    $args = array(
+        'label' => __('Zabieg', 'webs'),
+        'labels' => $labels,
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_position' => 4,
+        'show_in_admin_bar' => true,
+        'show_in_nav_menus' => true,
+        'can_export' => true,
+        'has_archive' => true,
+        'exclude_from_search' => false,
+        'publicly_queryable' => true,
+        'capability_type' => 'page',
+        // 'taxonomies' => array('category'),
+    );
+    
+    register_post_type('zabieg', $args);
+
+    register_taxonomy(
+        "zabiegi",
+        array("zabieg"),
+        array(
+            "hierarchical" => true,
+            "label" => "Kategorie",
+            "singular_label" => "Kategoria",
+            "rewrite" => true
+            )
+    );
+}
+add_action('init', 'post_type_zabiegi', 0);
+
+function post_type_team(){
+    $labels = array(
+        'name' => _x('Personel', 'Personel', 'webs'),
+        'singular_name' => _x('Personel', 'Personel', 'webs'),
+    );
+
+    $args = array(
+        'label' => __('Personel', 'webs'),
+        'labels' => $labels,
+        'supports' => array('title', 'editor', 'thumbnail'),
+        'hierarchical' => false,
+        'public' => true,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        'menu_position' => 5,
+        'show_in_admin_bar' => true,
+        'show_in_nav_menus' => true,
+        'can_export' => true,
+        'has_archive' => true,
+        'exclude_from_search' => false,
+        'publicly_queryable' => true,
+        'capability_type' => 'page',
+        // 'taxonomies' => array('category'),
+    );
+    
+    register_post_type('personel', $args);
+
+    register_taxonomy(
+        "team",
+        array("personel"),
+        array(
+            "hierarchical" => true,
+            "label" => "Kategorie",
+            "singular_label" => "Kategoria",
+            "rewrite" => true
+            )
+    );
+}
+add_action('init', 'post_type_team', 0);
