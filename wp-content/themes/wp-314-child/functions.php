a94638ea7758cd18542634f65796a2d4b23866b2
ghost717 <dawid.zawadzki7717@gmail.com>
2019-09-10 09:44:48 +0200

    styles

--- wp-content/themes/wp-314-child/functions.php
+++ wp-content/themes/wp-314-child/functions.php
@@ -3,8 +3,11 @@
 // load parent scripts & styles
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style', 99 );
 function enqueue_parent_theme_style() {
+    //custom styles
     wp_enqueue_style('child314css', get_stylesheet_directory_uri() . '/dist/css/app.css', true);
     wp_enqueue_script('child314js', get_stylesheet_directory_uri() . '/dist/js/app.js', false, '1', 'all');
+
+    //WP styles
     wp_enqueue_style('main-parent-style', get_template_directory_uri().'/style.css' );
     wp_enqueue_style('main-child-style', get_stylesheet_directory_uri().'/style.css' );
 }
